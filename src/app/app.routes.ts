442635f5463f03964a2aa8ac5ddd204f6be2793c
laptop-nsth4ium bagyasri <laptop-nsth4ium.bagyasri@default.com>
2025-09-14 13:23:25 +0530

    sign in component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import {SignIn} from './sign-in/sign-in'
 
 export const routes: Routes = [
     // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', component: Home }, ]
+  // { path: '', component: Home }, 
+  { path: '', component: SignIn}];
